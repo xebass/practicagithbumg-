c853d59718d9c7bef29b5de47002f0777ece2b57
umg <umg@JOC-LAB-1A1.labjoc.local>
2026-04-14 19:03:53 -0600

    Clase empleados manejara la informacion de los objetos empleados

---
+++ src/github/Empleado.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package github;
+
+/**
+ *
+ * @author umg
+ */
+public class Empleado {
+    int id;
+    String nombre;
+    String apellido;
+    String direccion;
+
+    public Empleado(int id, String nombre, String apellido, String direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+}
